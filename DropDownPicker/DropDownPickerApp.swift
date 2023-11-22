3fa45634c349520a54f1321edb2bcd7cb8648325
AlexisMic <amschotte@me.com>
2023-11-22 10:28:06 +0000

    Initial Commit

---
+++ DropDownPicker/DropDownPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownPickerApp.swift
+//  DropDownPicker
+//
+//  Created by Alexis Schotte on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DropDownPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
