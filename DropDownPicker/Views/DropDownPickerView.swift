b9b2fc6324ffe650dc8a61774fdf65469adf9aab
AlexisMic <amschotte@me.com>
2023-11-23 09:05:08 +0000

    Drop Down Picker complete

---
+++ DropDownPicker/Views/DropDownPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  DropDownPickerView.swift
+//  DropDownPicker
+//
+//  Created by Alexis Schotte on 22/11/2023.
+//
+
+import SwiftUI
+
+struct DropDownPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropDownPickerView()
+}
